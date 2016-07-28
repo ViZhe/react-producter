48724483c4cbfa1252edb782454cb9b09d373a01
ViZhe <vizhe.cat@ya.ru>
2016-07-28 21:26:25 +0300

    add protect from random destroy

--- source/containers/Options/OptionsFieldsListItem.js
+++ source/containers/Options/OptionsFieldsListItem.js
@@ -7,10 +7,13 @@ import * as hz from '~/utils/horizon/helpers'
 
 class OptionsFieldsListItemContainer extends Component {
   destroyField() {
-    hz.optionsFields.remove(this.props.field.id).subscribe(
-      result => console.info(`Destroy - Field - Success\nId: "${result.id}"`),
-      err => console.error(`Destroy - Field - Fail\nError: "${err}"`)
-    )
+    const isAgree = confirm(`ID: ${this.props.field.id}\nReally want to destroy the field?`)  // eslint-disable-line no-alert
+    if (isAgree) {
+      hz.optionsFields.remove(this.props.field.id).subscribe(
+        result => console.info(`Destroy - Field - Success\nId: "${result.id}"`),
+        err => console.error(`Destroy - Field - Fail\nError: "${err}"`)
+      )
+    }
   }
   render() {
     return <OptionsFieldsListItem
