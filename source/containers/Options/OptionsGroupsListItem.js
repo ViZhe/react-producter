e3803af8b450f078081bc9bd6a282bebd3e79a4d
ViZhe <vizhe.cat@ya.ru>
2016-08-01 21:43:16 +0300

    add destroy for OptionsGroupsListItem

---
+++ source/containers/Options/OptionsGroupsListItem.js
@@ -0,0 +1,27 @@
+
+import React, {Component} from 'react'
+
+import {OptionsGroupsListItem} from '~/components'
+import * as hz from '~/utils/horizon/helpers'
+
+
+class OptionsGroupsListItemContainer extends Component {
+  destroyGroup() {
+    const isAgree = confirm(`ID: ${this.props.group.id}\nReally want to destroy the group?`)  // eslint-disable-line no-alert
+    if (isAgree) {
+      hz.optionsGroups.remove(this.props.group.id).subscribe(
+        result => console.info(`Destroy - Group - Success\nId: "${result.id}"`),
+        err => console.error(`Destroy - Group - Fail\nError: "${err}"`)
+      )
+    }
+  }
+  render() {
+    return <OptionsGroupsListItem
+      destroyHandler={::this.destroyGroup}
+      {...this.props}
+      />
+  }
+}
+
+
+export default OptionsGroupsListItemContainer
