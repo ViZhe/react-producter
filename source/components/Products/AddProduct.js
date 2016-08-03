4b86e9b75d4426b693df6fa51b42c08548fdc548
ViZhe <vizhe.cat@ya.ru>
2016-08-03 22:31:22 +0300

    add select templare for AddProduct

--- source/components/Products/AddProduct.js
+++ source/components/Products/AddProduct.js
@@ -3,11 +3,24 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {reset} from 'redux-form'
 
+import {
+  loadOptionsTemplates
+} from '~/core/options/actions'
 import {DynamicFormCreator} from '~/components'
 import * as hz from '~/utils/horizon/helpers'
 
-
+/*eslint-disable*/
 class AddProduct extends Component {
+  componentWillMount() {
+    this.setState({currentTemplate: 'defaultTemplate'})
+    hz.optionsTemplates.watch().subscribe(response => {
+      this.props.dispatch(loadOptionsTemplates(response))
+    })
+
+  }
+  handleSelectTemplate = event => {
+    this.setState({currentTemplate: event.target.value})
+  }
   handleAddProduct = data => {
     if (!Object.keys(data).length) {
       console.info('handleAddProduct: Поля не изменились.')
@@ -26,12 +39,20 @@ class AddProduct extends Component {
     )
   }
   render() {
-    const templ = 'defaultTemplate'
-
+    const {options: {templates}} = this.props
     return <div className='c-area-list'>
       <h2>Add Product</h2>
+
+      <p>Select template:</p>
+      <select onChange={this.handleSelectTemplate}>
+        {templates.map(({name, title}) =>
+          <option value={name}>{title}</option>
+        )}
+      </select>
+
+      <p>Create product:</p>
       <DynamicFormCreator
-        template={templ}
+        template={this.state.currentTemplate}
         onSubmit={this.handleAddProduct}
         submitButtonText='Создать товар'
         />
@@ -40,11 +61,17 @@ class AddProduct extends Component {
 }
 
 
+const mapStateToProps = state => ({
+  options: {
+    templates: state.options.toJS().templates
+  }
+})
+
 const mapDispatchToProps = dispatch => ({
   resetForm: () => dispatch(reset('dynamicForm'))
 })
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(AddProduct)
