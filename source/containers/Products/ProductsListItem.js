bc6a5c867e55cc5c6bb804a33e66b00ab7349d5e
ViZhe <vizhe.cat@ya.ru>
2016-08-04 20:43:43 +0300

    add protect from destroy for ProductListItem

--- source/containers/Products/ProductsListItem.js
+++ source/containers/Products/ProductsListItem.js
@@ -24,10 +24,15 @@ class ProductsListItemContainer extends Component {
     this.updateProductField(product.id, 'isActivated', !product.isActivated)
   }
   destroyProduct = () => {
-    hz.products.remove(this.props.product.id).subscribe(
-      res => console.info(`Type: Product Destroy\nID: "${res.id}"`),
-      err => console.error('Destroy Fail', err)
-    )
+    const id = this.props.product.id
+    const isAgree = confirm(`ID: ${id}\nReally want to destroy the product?`) // eslint-disable-line no-alert
+
+    if (isAgree) {
+      hz.products.remove(this.props.product.id).subscribe(
+        res => console.info(`Type: Product Destroy\nID: "${res.id}"`),
+        err => console.error('Destroy Fail', err)
+      )
+    }
   }
   render() {
     return <ProductsListItem
