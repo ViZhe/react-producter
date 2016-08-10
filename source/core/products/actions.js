3db95c528e6f1d443b3167889a97acf6e486ee79
ViZhe <vizhe.cat@ya.ru>
2016-08-10 11:59:28 +0300

    core/products: update on the rules

--- source/core/products/actions.js
+++ source/core/products/actions.js
@@ -9,44 +9,32 @@ import {
 } from './constants'
 
 
-export const loadProducts = data => {
-  return {
-    type: LOAD_PRODUCTS,
-    payload: data
-  }
-}
+export const loadProducts = data => ({
+  type: LOAD_PRODUCTS,
+  payload: data
+})
 
-export const loadProduct = data => {
-  return {
-    type: LOAD_PRODUCT,
-    payload: data
-  }
-}
+export const loadProduct = data => ({
+  type: LOAD_PRODUCT,
+  payload: data
+})
 
-export const destroyProduct = data => {
-  return {
-    type: DESTROY_PRODUCT,
-    payload: data
-  }
-}
+export const destroyProduct = data => ({
+  type: DESTROY_PRODUCT,
+  payload: data
+})
 
-export const updateProductField = data => {
-  return {
-    type: UPDATE_PRODUCT_FIELD,
-    payload: data
-  }
-}
+export const updateProductField = data => ({
+  type: UPDATE_PRODUCT_FIELD,
+  payload: data
+})
 
-export const updateProduct = data => {
-  return {
-    type: UPDATE_PRODUCT,
-    payload: data
-  }
-}
+export const updateProduct = data => ({
+  type: UPDATE_PRODUCT,
+  payload: data
+})
 
-export const addProduct = data => {
-  return {
-    type: ADD_PRODUCT,
-    payload: data
-  }
-}
+export const addProduct = data => ({
+  type: ADD_PRODUCT,
+  payload: data
+})
