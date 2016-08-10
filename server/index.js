e94090d66a8051cf689b5da912e86b6e54990c9f
ViZhe <vizhe.cat@ya.ru>
2016-08-10 11:54:23 +0300

    server: update on the rules

--- server/index.js
+++ server/index.js
@@ -6,15 +6,16 @@ import webpack from 'webpack'
 import webpackDevMiddleware from 'webpack-dev-middleware'
 import webpackHotMiddleware from 'webpack-hot-middleware'
 import config from '../webpack/webpack.config.client.development'
+
 import {configDb} from './config.rethinkdb'
 
 
 const server = new Express()
 const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || '3000'
-const __PROD__ = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === 'production'
 
-const assets = __PROD__ ? require('../assets.json') : ''
+const assets = isProduction ? require('../assets.json') : ''
 
 const compiler = webpack(config)
 server.use(webpackDevMiddleware(compiler, {
@@ -35,12 +36,15 @@ server.get('*', (req, res) => {
     <html lang="ru-RU">
       <head>
         <meta charset="utf-8" >
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" >
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1,maximum-scale=1, user-scalable=no"
+        >
       </head>
       <body>
         <div id="root"></div>
-        <script src="${__PROD__ ? assets.vendor.js : '/vendor.js'}"></script>
-        <script async src="${__PROD__ ? assets.main.js : '/main.js'}" ></script>
+        <script src="${isProduction ? assets.vendor.js : '/vendor.js'}"></script>
+        <script async src="${isProduction ? assets.main.js : '/main.js'}" ></script>
       </body>
     </html>
   `)
@@ -52,7 +56,7 @@ const httpServer = server.listen(port, host, () =>
 
 # ==> Server was started.
 # ==> Link: http://${host}:${port}
-# ==> Mode: ${__PROD__ ? 'Production ' : 'Development'}
+# ==> Mode: ${isProduction ? 'Production ' : 'Development'}
 
 ######### ( ╯°□°)╯ ┻━━┻ #########
 `)
