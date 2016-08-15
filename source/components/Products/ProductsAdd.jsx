fc9357b9a58a42dd0c5898115203e1a32b062c7f
ViZhe <vizhe.cat@ya.ru>
2016-08-15 14:08:53 +0300

    add prettiness

--- source/components/Products/ProductsAdd.jsx
+++ source/components/Products/ProductsAdd.jsx
@@ -10,18 +10,18 @@ const ProductsAdd = ({
   <div>
     <h2>Add Product</h2>
 
-    <p>Select template:</p>
+    <p><b>Select template:</b></p>
     <select onChange={selectTemplateHandler}>
       {templates.map(({name, title}, index) =>
         <option key={index} value={name}>{title}</option>
       )}
     </select>
 
-    <p>Create product:</p>
+    <p><b>Create product:</b></p>
     <form onSubmit={handleSubmit} >
       {groups.map((group, groupIndex) => (
         <div key={groupIndex} >
-          <div>{group.title} - ({group.fields.length})</div>
+          <div>{groupIndex}) <b>{group.title} - ({group.fields.length})</b></div>
 
           {fields.filter(field => group.fields.indexOf(field.id) >= 0)
             .sort((a, b) => a.title > b.title) // TODO: ability to change the order
