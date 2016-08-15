510f46e20e2f61f2d428d3d8eca7b1ceb11dd8b7
ViZhe <vizhe.cat@ya.ru>
2016-08-15 12:27:58 +0300

    add FormRenderInput for OptionsGroups forms

--- source/components/Options/OptionsGroupsAdd.jsx
+++ source/components/Options/OptionsGroupsAdd.jsx
@@ -2,31 +2,28 @@
 import React from 'react'
 import {Field} from 'redux-form'
 
+import {FormRenderInput} from '~/components'
 
-const renderField = ({meta: {touched, error}, input, ...rest}) => (
-  <div>
-    <input {...rest} {...input} />
-    {touched && error && <span>{error}</span>}
-  </div>
-)
 
 const OptionsGroupsAdd = ({fields, handleSubmit}) => (
   <div>
     <h2>Options Groups Add</h2>
 
     <form onSubmit={handleSubmit} >
-      <div>
-        <label>Название</label>
-        <div>
-          <Field component={renderField} type='text' name='name' placeholder='Название' />
-        </div>
-      </div>
-      <div>
-        <label>Заголовок группы</label>
-        <div>
-          <Field component={renderField} type='text' name='title' placeholder='Заголовок группы' />
-        </div>
-      </div>
+      <Field
+        component={FormRenderInput}
+        title='Название'
+        type='text'
+        name='name'
+        placeholder='Название'
+      />
+      <Field
+        component={FormRenderInput}
+        title='Заголовок группы'
+        type='text'
+        name='title'
+        placeholder='Заголовок группы'
+      />
       <div>
         <label>Поля</label>
         <div>
