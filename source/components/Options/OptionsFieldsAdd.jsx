fd5d0e9fcaa6b38d9505673bba976a780d425ed2
ViZhe <vizhe.cat@ya.ru>
2016-08-15 13:57:23 +0300

    update OptionsFields for new FormField

--- source/components/Options/OptionsFieldsAdd.jsx
+++ source/components/Options/OptionsFieldsAdd.jsx
@@ -1,8 +1,7 @@
 
 import React from 'react'
-import {Field} from 'redux-form'
 
-import {FormRenderAuto} from '~/components'
+import {FormField} from '~/components'
 
 
 const OptionsFieldsAdd = ({validateList, handleSubmit}) => (
@@ -10,15 +9,13 @@ const OptionsFieldsAdd = ({validateList, handleSubmit}) => (
     <h2>Options Fields Add</h2>
 
     <form onSubmit={handleSubmit} >
-      <Field
-        component={FormRenderAuto}
+      <FormField
         title='Название'
         type='text'
         name='name'
         placeholder='Название'
       />
-      <Field
-        component={FormRenderAuto}
+      <FormField
         title='Тип поля'
         type='select'
         name='type'
@@ -27,23 +24,20 @@ const OptionsFieldsAdd = ({validateList, handleSubmit}) => (
         <option value='' >Select a type...</option>
         <option value='text' >text</option>
         <option value='textarea' >textarea</option>
-      </Field>
-      <Field
-        component={FormRenderAuto}
+      </FormField>
+      <FormField
         title='Заголовок поля'
         type='text'
         name='title'
         placeholder='Заголовок поля'
       />
-      <Field
-        component={FormRenderAuto}
+      <FormField
         title='Значение по умолчанию'
         type='text'
         name='defaultValue'
         placeholder='Значение по умолчанию'
       />
-      <Field
-        component={FormRenderAuto}
+      <FormField
         title='Заполнитель'
         type='text'
         name='placeholder'
@@ -55,22 +49,19 @@ const OptionsFieldsAdd = ({validateList, handleSubmit}) => (
           <div key={index} style={{border: '1px solid'}} >
             <div>{index}) <b>{name}</b></div>
             <div>
-              <Field
-                component={FormRenderAuto}
+              <FormField
                 title='Активный'
                 type='checkbox'
                 name={`validate.${name}.isActive`}
               />
-              <Field
-                component={FormRenderAuto}
+              <FormField
                 title='Текст ошибки'
                 type='text'
                 name={`validate.${name}.title`}
                 placeholder='Текст ошибки'
               />
               {name === 'regex' &&
-                <Field
-                  component={FormRenderAuto}
+                <FormField
                   title='Регулярное выражение'
                   type='text'
                   name={`validate.${name}.regex`}
