3ad4e258bf228fc523c184847e523a123d7ac272
ViZhe <vizhe.cat@ya.ru>
2016-08-22 21:00:07 +0300

    add check latin symbols for name

--- source/utils/options/validationFields.js
+++ source/utils/options/validationFields.js
@@ -7,11 +7,13 @@ const createValidate = values => {
     }
   }
 
-  // TODO:
-  // name: only latin symbols
-  // all: length? 20?
   if (!values.name) {
     errors.name = 'Required'
+  } else {
+    const regex = new RegExp('^[a-z]+$', 'i')
+    if (!regex.test(values.name)) {
+      errors.name = 'Must be: a-z'
+    }
   }
   if (!values.type) {
     errors.type = 'Required'
