2c2687f39a521e548e538b409c91f62a0459f496
ViZhe <vizhe.cat@ya.ru>
2016-08-27 15:34:33 +0300

    filled ProductListItem

---
+++ source/components/Product/List/Item/index.jsx
@@ -0,0 +1,42 @@
+
+import React, {PropTypes} from 'react'
+import {Link} from 'react-router'
+
+
+const ProductListItem = ({product, handleToggleDelete, handleToggleActive, handleDestroy}) => {
+  let tdStyle = {
+    border: '1px solid gray'
+  }
+
+  const id = product.id
+
+  return (
+    <tr>
+      <td style={tdStyle} >{id}</td>
+      <td style={tdStyle} >{product.options && product.options.name}</td>
+      <td style={tdStyle} >{product.template}</td>
+      <td style={tdStyle} ><Link to={`/products/${id}/edit`} >edit</Link></td>
+      <td style={tdStyle} >
+        <button onClick={handleToggleDelete} >
+          {product.isDeleted ? 'restore' : 'detele'}
+        </button>
+      </td>
+      <td style={tdStyle} >
+        <button onClick={handleToggleActive} >
+          {product.isActivated ? 'deactivate' : 'activate'}
+        </button>
+      </td>
+      <td style={tdStyle} ><button onClick={handleDestroy} >destroy</button></td>
+    </tr>
+  )
+}
+
+
+ProductListItem.propTypes = {
+  product: PropTypes.object.isRequired,
+  handleToggleDelete: PropTypes.func.isRequired,
+  handleToggleActive: PropTypes.func.isRequired,
+  handleDestroy: PropTypes.func.isRequired
+}
+
+export default ProductListItem
