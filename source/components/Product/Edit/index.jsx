fe00086df7ebe5b0a3ec9640530bfd5337e13a04
ViZhe <vizhe.cat@ya.ru>
2016-08-27 15:49:19 +0300

    filled ProductEdit

---
+++ source/components/Product/Edit/index.jsx
@@ -0,0 +1,43 @@
+
+import React from 'react'
+
+import {FormField} from '~/components'
+
+
+const ProductEdit = ({isLoading, groups, fields, handleSubmit, textSubmitButton}) => {
+  const empty = <div>Данные загружаются</div>
+  const form = (
+    <form onSubmit={handleSubmit} >
+      {groups.map((group, groupIndex) => (
+        <div key={groupIndex} >
+          <div>{groupIndex}) <b>{group.title} - ({group.fields.length})</b></div>
+
+          {fields.filter(field => group.fields.indexOf(field.id) >= 0)
+            .sort((a, b) => a.title > b.title) // TODO: ability to change the order
+            .map(({type, name, title, placeholder}, fieldIndex) => (
+              <FormField
+                key={fieldIndex}
+                title={title}
+                type={type}
+                name={name}
+                placeholder={placeholder}
+                // normalize={value => value.toUpperCase()}
+              />
+            )
+          )}
+        </div>
+      ))}
+      <button>{textSubmitButton}</button>
+    </form>
+  )
+
+  return (
+    <div>
+      <h2>Product Edit</h2>
+      {isLoading ? empty : form}
+    </div>
+  )
+}
+
+
+export default ProductEdit
