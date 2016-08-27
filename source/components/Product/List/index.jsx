eb792873e503e18fcf9abce84daf18144983911c
ViZhe <vizhe.cat@ya.ru>
2016-08-27 15:34:40 +0300

    filled ProductList

---
+++ source/components/Product/List/index.jsx
@@ -0,0 +1,49 @@
+
+import React, {PropTypes} from 'react'
+
+import {ProductListItem} from '~/containers'
+
+
+const ProductsList = ({products}) => {
+  let tableStyle = {
+    border: '1px solid black'
+  }
+
+  const productsList = products.data.map((product, index) =>
+    <ProductListItem
+      key={index}
+      product={product}
+    />
+  )
+  const productsEmpty = <tr><td colSpan='7' >Товаров нет</td></tr>
+  const productsLoading = <tr><td colSpan='7' >Товары загружаются</td></tr>
+
+  return (
+    <div className='c-products-list' >
+      <h2>Products List</h2>
+      <table style={tableStyle} >
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>o.name</th>
+            <th>template</th>
+            <th>edit</th>
+            <th>isDeleted</th>
+            <th>isActivated</th>
+            <th>destroy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.isLoading ? productsLoading : productsList || productsEmpty}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+
+ProductsList.propTypes = {
+  products: PropTypes.object.isRequired
+}
+
+export default ProductsList
