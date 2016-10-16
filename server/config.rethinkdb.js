b9d8fc95be227965727f6780d13b5d03abe43a28
ViZhe <vizhe.cat@ya.ru>
2016-10-16 16:46:32 +0300

    share rethink config

---
+++ server/config.rethinkdb.js
@@ -0,0 +1,13 @@
+
+/*eslint-disable */
+export const configDb = {
+  project_name: 'Hoppas',
+  permissions: false,
+  auto_create_collection: true,
+  auto_create_index: true,
+  auth:{
+    allow_anonymous: true,
+    allow_unauthenticated: true,
+    token_secret: 'VjZOapPjR1AX38xNliTAkk3BWaZUYyUMqFIMmO/98SzZCcluTxV8Sx2YHD/zmmJ21YULkEk0nr0Uf9NLh0MPAQ=='
+  }
+}
