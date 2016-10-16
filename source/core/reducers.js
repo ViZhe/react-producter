04a299ba92b40d2d1cd2d948d1fb8258fd71bd4f
ViZhe <vizhe.cat@ya.ru>
2016-10-16 16:00:12 +0300

    enable reducers

--- source/core/reducers.js
+++ source/core/reducers.js
@@ -2,12 +2,18 @@
 import {combineReducers} from 'redux'
 import {reducer as formReducer} from 'redux-form'
 
+import {productReducer} from './product'
+import {productTemplateReducer} from './productTemplate'
+
 import {optionsReducer} from './options'
 import {productsReducer} from './products'
 
 
 export default combineReducers({
   form: formReducer,
+  productTemplate: productTemplateReducer,
+  product: productReducer,
+
   options: optionsReducer,
   products: productsReducer
 })
